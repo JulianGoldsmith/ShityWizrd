0d6f6c3ac39f77be896b5d81ed101045da41a703
huesiq <huesiq@outlook.com>
2025-10-03 12:50:02 +0100

    better goo (stickiness)

--- Assets/Scripts/PhysicsSystem/PhysicsObject.cs
+++ Assets/Scripts/PhysicsSystem/PhysicsObject.cs
@@ -1,3 +1,4 @@
+using System.Timers;
 using UnityEngine;
 
 public class PhysicsObject : MonoBehaviour
@@ -117,8 +118,126 @@ public class PhysicsObject : MonoBehaviour
         // At about 10, it actually becomes sticky.
         // Might need a maxmin.
         //rigidbody.AddForce(-rigidbody.linearVelocity * physicsObjectProperties.stickiness);
-        rigidbody.linearVelocity = rigidbody.linearVelocity * Mathf.Clamp01(1 - physicsObjectProperties.stickiness);
+        //rigidbody.linearVelocity = rigidbody.linearVelocity * Mathf.Clamp01(1 - physicsObjectProperties.stickiness);
+        OnStick(other);
+    }
+    #endregion
+
+    #region Material Physics
+    void OnStick(Collider other)
+    {
+        // If I'm not sticky, don't do anything.
+        if (physicsObjectProperties.stickiness == 0)
+            return;
+
+        // If this is sticky and the halo has triggered, then:
+        // - Check if the other is a physicsobject
+        // - If yes, get it's rigidbody
+        // - Distribute shared momentum between us based on 
+        //      mass and stickiness.
+        // - If other is not physicsobject, then treat it
+        //      as a stationary,infinite mass.
+
+        // We adjust velocity for both ourselves and what we
+        // collided with.
+        // This may be wrong and might be funky over network
+        // due to authority...
+
+        // What about cases where both are sticky? If both
+        // are triggering haloes then we'd get double-stuck, which is
+        // fine, but ordering of application might lead to different
+        // results?
+
+        // In this current version, nothing sticks absolutely.
+        // Since the prior timestep will cause the object to fall
+        // one frame, then it will hit this trigger and zero-out its
+        // velocity. But it has already moved in that time, stupidly.
+
+        PhysicsObject other_po = other.GetComponent<PhysicsObject>();
+
+        Rigidbody other_rb = null;
+        float other_mass = float.MaxValue;
+        Vector3 other_velocity = Vector3.zero;
+        float other_stickiness = 0;
+
+        if(other_po != null)
+        {
+            if (other_po.rigidbody != null)
+            {
+                other_rb = other_po.rigidbody;
+                other_velocity = other_rb.linearVelocity;
+            }
+            if (other_po.physicsObjectProperties != null)
+            {
+                other_mass = other_po.physicsObjectProperties.mass;
+                if (other_po.physicsObjectProperties.physicsobjectmaterial != null)
+                    other_stickiness = other_po.physicsObjectProperties.physicsobjectmaterial.stickiness;
+            }
+        }
+
+        // QUESTION: should we be including the other's stickiness? They'll run their
+        // own calc anyway, to also apply their own stickiness?
+        // Another option is we track, within the currently frame, who we've already stuck
+        // to and then only let one of them run the calc.
+
+        // Here we combine stickiness.
+        float shared_stickiness_factor = Mathf.Clamp01(physicsObjectProperties.stickiness + other_stickiness);
         
+
+        float total_mass = physicsObjectProperties.mass + other_mass;
+
+        // Here's some standard momentum sharing physics:
+        Vector3 my_momentum = rigidbody.linearVelocity * physicsObjectProperties.mass;
+        Vector3 other_momentum = other_velocity * other_mass;
+        Vector3 total_momentum = my_momentum + other_momentum;
+
+
+        // we then distribute out the momenta back to the two objects based on mass
+        // and stickiness.
+        Vector3 my_new_momentum = my_momentum * (1 - shared_stickiness_factor) + 
+            total_momentum * shared_stickiness_factor * physicsObjectProperties.mass / total_mass;
+
+        rigidbody.linearVelocity = my_new_momentum / physicsObjectProperties.mass;
+
+        if (other_po != null)
+        {
+            // Also deal with the other.
+            Vector3 other_new_momentum = other_momentum * (1 - shared_stickiness_factor) +
+                total_momentum * shared_stickiness_factor * other_mass / total_mass;
+            other_rb.linearVelocity = other_new_momentum / other_mass;
+        }
+
+        // Sense checking:
+        // stickiness = 0 (no stick).
+        // my_new_momentum = my_momentum
+        // other_new_momentum = other_momentum
+
+        // stickiness = 1 (full stick)
+        // my_new_momentum = total_momentum * my_mass / total_mass
+        // other_new_momentum = total_momentum * other_mass / total_mass
+        // my_new_velocity = total_momentum / total_mass
+        // other_new_velocity = total_momentum / total_mass
+        // -> this means both objects leave the collision
+        // with the same velocity, i.e. complete sticking.
+
+        // stationary wall. other_momentum = 0, other_velocity = 0, other_mass >> my_mass
+        // so therefore total_mass ~ other_mass
+        // and total_momentum = my_momentum
+        // my_new_momentum = my_momentum * my_mass / other_mass
+        // (other_new_momentum = my_momentum * other_mass / other_mass)
+        // my_new_velocity = my_velocity * my_mass / other_mass -> 0 as other_mass -> Infinity
+        // so complete sticking to wall.
+
+        // Stationary wall, stickiness = 0.5
+        // other_momentum = 0, other_velocity = 0, other_mass >> my_mass
+        // so therefore total_mass ~ other_mass
+        // my_new_momentum = 0.5 * my_momentum * (1 + my_mass / other_mass)
+        // (other_new_momentum = 0.5 * my_momentum * other_mass / other_mass)
+        // my_new_velocity = 0.5 * my_velocity * (
+
+        // All looks good.
+
+
     }
     #endregion
 
